b13778d75920a4700b9f315b30e0671ef1f4e42a
gunkarlsson <hi@gunkarlsson.com>
2021-09-21 10:04:20 +0200

    ✨  got query filtering in firestore to work

--- src/components/Ads/MyAds.js
+++ src/components/Ads/MyAds.js
@@ -5,51 +5,62 @@ import { useAuth } from "../../context/AuthContext";
 import { PrimarySection, AdForm } from "../../style/StyledComponents";
 
 const MyAds = () => {
-  const [ads, setAds] = useState([]);
+  const [myAds, setMyAds] = useState([]);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [loading, setLoading] = useState(false);
 
   const { currentUser } = useAuth();
   const currentUserId = currentUser ? currentUser.uid : null;
-  const [myAds, setMyAds] = useState([]);
-
-  // useEffect(() => {
-  //   db.collection("users")
-  //     .doc(currentUser.uid)
-  //     .collection("myAds")
-  //     .get()
-  //     //this is async, so it returns a promise
-  //     .then((snapshot) => {
-  //       let documents = [];
-  //       snapshot.docs.forEach((doc) => {
-  //         documents.push(doc.data());
-  //       });
-  //       setMyAds(documents);
-  //     });
-  // }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, desc);
     setTitle("");
     setDesc("");
   };
+
   //TUTORIAL WAY: BETTER PERFORMANCE + INSTANT UPDATE
+
+  // db.collection("allAds")
+  //   .where("userId", "==", "7xtCQXwFR3OfRQYGHECYxIJfkN02")
+  //   .get()
+  //   .then(function (querySnapshot) {
+  //     querySnapshot.forEach(function (doc) {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       console.log(doc.id, " => ", doc.data());
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error getting documents: ", error);
+  //   });
+
   const ref = db.collection("allAds");
 
   const getAds = () => {
     setLoading(true);
-    ref.onSnapshot((querySnapshot) => {
+    ref.where("userId", "==", currentUserId).onSnapshot((querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
         items.push(doc.data());
       });
       setMyAds(items);
       setLoading(false);
-      console.log(currentUser.email, currentUser.name);
+      console.log(currentUserId, myAds);
     });
   };
+
+  // const getAds = () => {
+  //   setLoading(true);
+  //   ref.onSnapshot((querySnapshot) => {
+  //     const items = [];
+  //     querySnapshot.forEach((doc) => {
+  //       items.push(doc.data());
+  //     });
+  //     setMyAds(items);
+  //     setLoading(false);
+  //     console.log(currentUser.email, currentUser.name);
+  //   });
+  // };
 
   useEffect(() => {
     getAds();
